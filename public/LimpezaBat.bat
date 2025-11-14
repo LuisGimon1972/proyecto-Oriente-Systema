707671518be1d55b9c2ba4bee63c5bc5fb765144
Luis Manuel <luismanuel@SGBR.intra>
2025-11-14 16:57:59 -0300

    Nuevo arquivo

---
+++ public/LimpezaBat.bat
@@ -0,0 +1,14 @@
+@echo Limpando
+C:
+cd\
+cd sgbr1
+cd master
+del *.tmp
+del bac*.*
+rmdir /s /q C:\sgbr1\master\logs
+rmdir /s /q C:\sgbr1\master\backup
+rmdir /s /q C:\sgbr1\master\auditoriabd
+del *.zip
+pausa
+
+
